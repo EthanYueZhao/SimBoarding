ea8faea30e61a0c0cdb41420be22884553e6450e
Ethan Yue Zhao <xishanlake@gmail.com>
2015-04-05 18:29:03 -0400

    finished seats

--- Plane.java
+++ Plane.java
@@ -9,8 +9,8 @@ import java.util.*;
  */
 public class Plane extends World
 {
- private static final int SEAT_NUMBER=137;
- private ArrayList<Seat> seats=new ArrayList();
+    private static final int SEAT_NUMBER=137;
+    private ArrayList<Seat> seats=new ArrayList();
     /**
      * Constructor for objects of class plane.
      * 
@@ -29,28 +29,102 @@ public class Plane extends World
      */
     private void prepare()
     {
-        Seat seat=new Seat(200,200);
-            
-          for(int i=0;i<SEAT_NUMBER;i++){
-            Passenger p=new Passenger(seat);
+        Collections.shuffle(seats);
+        for(int i=0;i<SEAT_NUMBER;i++){
+            Seat s=seats.get(i);
+            Passenger p=new Passenger(s);
             int x=10+i*20;
             int y=600;
             if(x>=1380){
                 x=x-1380;
                 y=y+30;
             }
-                        
+
             addObject(p, x, y);
         }
-  
+
     }
-    
+
     private void initSeats()
     {
-        for(int i=0;i<6;i++)
+        for(int i=0;i<3;i++)
         {
-            Seat s=new Seat(200,340);
-            seats.add(s);
+            int x=200+i*45;
+            int a=340;
+            int b=300;
+            int c=235;
+            int d=195;            
+            Seat s1=new Seat(x,a);
+            seats.add(s1);
+            Seat s2=new Seat(x,b);
+            seats.add(s2); 
+            Seat s3=new Seat(x,c);
+            seats.add(s3); 
+            Seat s4=new Seat(x,d);
+            seats.add(s4);
         }
+        for(int i=0;i<7;i++)
+        {
+            int x=340+i*43;
+            int a=350;
+            int b=325;
+            int c=300;
+            int d=245;
+            int e=215;
+            int f=190;
+            Seat s1=new Seat(x,a);
+            seats.add(s1);
+            Seat s2=new Seat(x,b);
+            seats.add(s2); 
+            Seat s3=new Seat(x,c);
+            seats.add(s3); 
+            Seat s4=new Seat(x,d);
+            seats.add(s4);
+            Seat s5=new Seat(x,e);
+            seats.add(s5);
+            Seat s6=new Seat(x,f);
+            seats.add(s6);
+        }
+        for(int i=0;i<13;i++)
+        {
+            int x=650+i*42;
+            int a=350;
+            int b=325;
+            int c=300;
+            int d=245;
+            int e=215;
+            int f=190;
+            Seat s1=new Seat(x,a);
+            seats.add(s1);
+            Seat s2=new Seat(x,b);
+            seats.add(s2); 
+            Seat s3=new Seat(x,c);
+            seats.add(s3); 
+            Seat s4=new Seat(x,d);
+            seats.add(s4);
+            Seat s5=new Seat(x,e);
+            seats.add(s5);
+            Seat s6=new Seat(x,f);
+            seats.add(s6);
+        }
+        for(int i=0;i<1;i++)
+        {
+            int x=1190;
+            int a=350;
+            int b=325;
+            int c=300;
+            int d=235;
+            int e=210;            
+            Seat s1=new Seat(x,a);
+            seats.add(s1);
+            Seat s2=new Seat(x,b);
+            seats.add(s2); 
+            Seat s3=new Seat(x,c);
+            seats.add(s3); 
+            Seat s4=new Seat(x,d);
+            seats.add(s4);
+            Seat s5=new Seat(x,e);
+            seats.add(s5);           
+        }        
     }
 }
