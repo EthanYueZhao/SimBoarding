727fe1f0e1fc915988d27ea6a8ee8b0c05479fde
Ethan Yue Zhao <xishanlake@gmail.com>
2015-04-06 16:15:12 -0400

    finished Idle state

--- Passenger.java
+++ Passenger.java
@@ -12,6 +12,9 @@ public class Passenger extends Actor
     public static final int STATE_IDLE=0;
     public static final int STATE_SEATED=1;
     public static final int STATE_MOVING=2;
+    public static final int STATE_PUTTING=3;
+    public static final int STATE_ArrivedPoint1=4;
+    public static final int STATE_ArrivedPoint2=5;
     public int current_state;
     private Seat destination;
     private Point checkPoint1=new Point(140,530);
@@ -19,12 +22,15 @@ public class Passenger extends Actor
     private Point checkPoint3;
     private Point targetPoint;
     private int move_speed;
+    private int putting_time;
+    private int timer=0;
     public Passenger(Seat destination){
         this.destination=destination;
         this.current_state=STATE_IDLE;
         checkPoint3=new Point(destination.x-20,270);
         targetPoint=checkPoint1;
         move_speed= Greenfoot.getRandomNumber(4)+1;
+        putting_time=Greenfoot.getRandomNumber(1000)+100;
     }
 
     private void addedToWorld(){
@@ -38,15 +44,12 @@ public class Passenger extends Actor
 
     public void act() 
     {
+        //turnTowards(targetPoint.x,targetPoint.y);
         switch(current_state)
         {
             case STATE_IDLE:
-            checkCurrentPoint();
             turnTowards(targetPoint.x,targetPoint.y);
-                        if(!checkBlocked())
-            {
-                current_state=STATE_MOVING;
-            }
+            idle();                   
             break;
             case STATE_MOVING: 
             checkCurrentPoint();
@@ -57,6 +60,17 @@ public class Passenger extends Actor
                 current_state=STATE_IDLE; 
             }
             break;
+            case STATE_ArrivedPoint1:
+            turnTowards(targetPoint.x,targetPoint.y);
+            arrivedCheckPoint1();
+            break;
+            case STATE_ArrivedPoint2:
+            turnTowards(targetPoint.x,targetPoint.y);
+            arrivedCheckPoint2();
+            break;
+            case STATE_PUTTING:
+            putLuggage();
+            break;
 
             case STATE_SEATED:
             return;
@@ -64,10 +78,22 @@ public class Passenger extends Actor
         }       
     }
 
+    private void putLuggage()
+    {
+        if(timer<putting_time)
+        {
+            timer++;
+        }else
+        {
+            current_state=STATE_MOVING;
+            timer=0;
+        }
+    }
+          
     private boolean checkBlocked()
     {
         //Actor p =getOneObjectAtOffset(2,1 , Passenger.class);
-        List<Passenger> ps= getIntersectingObjects( Passenger.class);
+        List<Passenger> ps= getObjectsInRange(20, Passenger.class);
         if(!ps.isEmpty())
         {   
             for(Passenger p: ps)   
@@ -86,6 +112,45 @@ public class Passenger extends Actor
         return false;
     }
 
+    private void arrivedCheckPoint1()
+    {
+        Actor p =getOneObjectAtOffset(0,-15 , Passenger.class); 
+        if(p!=null)
+        {
+            return;
+        }else
+        {
+            move(1);
+            checkCurrentPoint();
+        }
+    }
+    
+    private void arrivedCheckPoint2()
+    {
+         Actor p =getOneObjectAtOffset(15,0 , Passenger.class); 
+        if(p!=null)
+        {
+            return;
+        }else
+        {
+            move(1);
+            checkCurrentPoint();
+        }
+    }
+
+    private void idle()
+    {
+         Actor p =getOneObjectAtOffset(-15,0 , Passenger.class); 
+        if(p!=null)
+        {
+            return;
+        }else
+        {
+            move(1);
+            checkCurrentPoint();
+        }
+    }
+    
     private void checkCurrentPoint()
     {
         int x= getX();
@@ -93,15 +158,18 @@ public class Passenger extends Actor
         if(x==checkPoint1.x & y==checkPoint1.y)
         {
             targetPoint=checkPoint2;
+            current_state=STATE_ArrivedPoint1;
         }
         if(x==checkPoint2.x & y==checkPoint2.y)
         {
             targetPoint=checkPoint3;
+            current_state=STATE_ArrivedPoint2;
         }
         if(x==checkPoint3.x & y==checkPoint3.y)
         {
             targetPoint.x=destination.x;
             targetPoint.y=destination.y;
+            current_state=STATE_PUTTING;
         }
         if(x==destination.x && y==destination.y)
         {
