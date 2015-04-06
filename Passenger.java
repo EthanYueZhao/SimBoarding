b1187a6122cbc676c1eb2b0831b76acdd0ecfc5d
Ethan Yue Zhao <xishanlake@gmail.com>
2015-04-06 00:09:27 -0400

    working on check collision

--- Passenger.java
+++ Passenger.java
@@ -9,17 +9,27 @@ import greenfoot.*;
 public class Passenger extends Actor
 {
     public static final int STATE_IDLE=0;
-    private int state;
+    public static final int STATE_SEATED=1;
+    public static final int STATE_MOVING=2;
+    private int current_state;
     private Seat destination;
+    private Point checkPoint1=new Point(145,500);
+    private Point checkPoint2=new Point(145,267);
+    private Point checkPoint3;
+    private Point targetPoint;
+    private int move_speed;
     public Passenger(Seat destination){
         this.destination=destination;
-        this.state=STATE_IDLE;
+        this.current_state=STATE_IDLE;
+        checkPoint3=new Point(destination.x-20,267);
+        targetPoint=checkPoint1;
+        move_speed= Greenfoot.getRandomNumber(4);
     }
 
     private void addedToWorld(){
-       
+
     }
-    
+
     /**
      * Act - do whatever the passenger wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -27,14 +37,57 @@ public class Passenger extends Actor
 
     public void act() 
     {
-        switch(state)
+        switch(current_state)
         {
             case STATE_IDLE:
-               turnTowards(destination.x,destination.y);
-               move(1);
-               break;
-           
+            checkBlocked();
+            break;
+            case STATE_MOVING:            
+            checkCurrentPoint();
+            turnTowards(targetPoint.x,targetPoint.y);
+            move(1);
+            checkBlocked();
+            break;
+
+            case STATE_SEATED:
+            break;
+
         }
-      // Add your action code here.
-    }    
+        // Add your action code here.
+    }
+
+    private void checkBlocked()
+    {
+        Actor p = getOneObjectAtOffset(17, 0, Passenger.class);
+        if(p!=null)
+        {    
+            current_state=STATE_IDLE;   
+        }else
+        {
+            current_state=STATE_MOVING;  
+        }
+    }
+
+    private void checkCurrentPoint()
+    {
+        int x= getX();
+        int y= getY();
+        if(x==checkPoint1.x & y==checkPoint1.y)
+        {
+            targetPoint=checkPoint2;
+        }
+        if(x==checkPoint2.x & y==checkPoint2.y)
+        {
+            targetPoint=checkPoint3;
+        }
+        if(x==checkPoint3.x & y==checkPoint3.y)
+        {
+            targetPoint.x=destination.x;
+            targetPoint.y=destination.y;
+        }
+        if(x==destination.x & y==destination.y)
+        {
+            current_state=STATE_SEATED;
+        }
+    }
 }
---
+++ Point.java
@@ -0,0 +1,26 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Point here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Point extends Actor
+{
+    public int x;
+    public int y;
+    public Point(int x, int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+    /**
+     * Act - do whatever the Point wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
