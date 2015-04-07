497bb8be37833e41fa12bf8c5746fb5e06607434
Ethan Yue Zhao <xishanlake@gmail.com>
2015-04-07 13:48:38 -0400

    working on waiting2()

--- Passenger.java
+++ Passenger.java
@@ -15,7 +15,8 @@ public class Passenger extends Actor
     public static final int STATE_PUTTING=3;
     public static final int STATE_ArrivedPoint1=4;
     public static final int STATE_ArrivedPoint2=5;
-    public static final int STATE_WAITING=6;
+    public static final int STATE_WAITING1=6;
+    public static final int STATE_WAITING2=7;
     public int current_state;
     private Seat destination;
     private Point checkPoint1=new Point(140,530);
@@ -26,18 +27,17 @@ public class Passenger extends Actor
     private int putting_time;
     private int timer=0;
     public Passenger notifier;
+    public Passenger notifyee1;
+    public Passenger notifyee2;
+
     public Passenger(Seat destination){
         this.destination=destination;
         this.current_state=STATE_IDLE;
         checkPoint3=new Point(destination.x-20,270);
-        targetPoint=checkPoint1;
+        targetPoint=new Point(checkPoint1.x,checkPoint1.y);
         move_speed= Greenfoot.getRandomNumber(4)+1;
         putting_time=Greenfoot.getRandomNumber(100);
         this.notifier=null;
-    }
-
-    private void addedToWorld(){
-
     }
 
     /**
@@ -54,26 +54,34 @@ public class Passenger extends Actor
             turnTowards(targetPoint.x,targetPoint.y);
             idle();                   
             break;
+
             case STATE_MOVING:  
             destination.occupied=false;
             checkCurrentPoint();
             turnTowards(targetPoint.x,targetPoint.y);
             move(1);
-                        
             break;
+
             case STATE_ArrivedPoint1:
             turnTowards(targetPoint.x,targetPoint.y);
             arrivedCheckPoint1();
             break;
+
             case STATE_ArrivedPoint2:
             turnTowards(targetPoint.x,targetPoint.y);
             arrivedCheckPoint2();
             break;
+
             case STATE_PUTTING:
             putLuggage();
             break;
-            case STATE_WAITING:
 
+            case STATE_WAITING1:
+            waiting1();
+            break;
+
+            case STATE_WAITING2:
+            waiting2();
             break;
 
             case STATE_SEATED:
@@ -81,6 +89,38 @@ public class Passenger extends Actor
             break;
 
         }       
+    }
+
+    private void waiting1()
+    {
+        int notifyee_number=0;
+        if(notifyee1!=null)
+        {
+            notifyee_number++;           
+        }
+        if(notifyee2!=null)
+        {
+            notifyee_number++; 
+        }
+        switch( notifyee_number)
+        {
+            case 1:
+            if(notifyee1.current_state==Passenger.STATE_WAITING2 | notifyee2.current_state==Passenger.STATE_WAITING2)
+            { current_state=STATE_MOVING;}
+            break;
+            case 2:
+            if(notifyee1.current_state==Passenger.STATE_WAITING2 & notifyee2.current_state==Passenger.STATE_WAITING2)
+            { current_state=STATE_MOVING;}
+            break;
+        }
+    }
+
+    private void waiting2()
+    {
+        if(notifier.current_state==Passenger.STATE_SEATED)
+        {
+            current_state=STATE_MOVING;
+        }
     }
 
     private void putLuggage()
@@ -115,8 +155,7 @@ public class Passenger extends Actor
             break;
             // economy class
             case 350:
-            checkSeated(destination.x,325);
-            checkSeated(destination.x,300);
+            checkSeated2(destination.x,325,300);
             break;
             case 325:
             checkSeated(destination.x,300);
@@ -131,8 +170,7 @@ public class Passenger extends Actor
             checkSeated(destination.x,245);
             break;
             case 190:
-            checkSeated(destination.x,245);
-            checkSeated(destination.x,215);
+            checkSeated2(destination.x,245,215);            
             break;
             // last row
             case 210:
@@ -149,37 +187,65 @@ public class Passenger extends Actor
             current_state=STATE_MOVING;
         }else
         {
-            //ps.get(0).moveOut(this);
-            Passenger p=ps.get(0);            
-            p.targetPoint.x=10;
-            p.targetPoint.y=270; 
-            p.notifier=this;
-            p.current_state=Passenger.STATE_MOVING;
-            current_state=STATE_WAITING;
+            notifyee1=ps.get(0);            
+            notifyee1.targetPoint.x=destination.x-15;
+            notifyee1.targetPoint.y=270; 
+            notifyee1.notifier=this;
+            notifyee1.current_state=Passenger.STATE_MOVING;
+            current_state=STATE_WAITING1;
         }
     }
 
-//     private boolean checkBlocked()
-//     {
-//         //Actor p =getOneObjectAtOffset(2,1 , Passenger.class);
-//         List<Passenger> ps= getObjectsInRange(20, Passenger.class);
-//         if(!ps.isEmpty())
-//         {   
-//             for(Passenger p: ps)   
-//             {   
-//                 if(p.getX()>this.getX() & p.getX()<this.destination.x & p.getY()>this.getY() & p.getY()<this.destination.y
-//                 || p.getX()>this.getX() & p.getX()<this.destination.x & p.getY()<this.getY() & p.getY()>this.destination.y
-//                 ) 
-//                 {
-//                     return true;                    
-//                 }else
-//                 {
-//                     continue;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
+    private void checkSeated2(int x, int y1,int y2)
+    {
+        List<Passenger> ps1= getWorld().getObjectsAt(x,y1, Passenger.class);
+        List<Passenger> ps2= getWorld().getObjectsAt(x,y2, Passenger.class);
+        if(!ps1.isEmpty())
+        {
+            notifyee1=ps1.get(0);
+            notifyee1.targetPoint.x=destination.x-15;
+            notifyee1.targetPoint.y=270; 
+            notifyee1.notifier=this;
+            notifyee1.current_state=Passenger.STATE_MOVING;
+        }
+        if(!ps2.isEmpty())
+        {
+            notifyee2=ps2.get(0);
+            notifyee2.targetPoint.x=destination.x-15;
+            notifyee2.targetPoint.y=270; 
+            notifyee2.notifier=this;
+            notifyee2.current_state=Passenger.STATE_MOVING;
+        }
+        if(notifyee1 ==null & notifyee2==null)
+        {
+            current_state=STATE_MOVING;
+        }else
+        {
+            current_state=STATE_WAITING1;
+        }
+    }
+
+    //     private boolean checkBlocked()
+    //     {
+    //         //Actor p =getOneObjectAtOffset(2,1 , Passenger.class);
+    //         List<Passenger> ps= getObjectsInRange(20, Passenger.class);
+    //         if(!ps.isEmpty())
+    //         {   
+    //             for(Passenger p: ps)   
+    //             {   
+    //                 if(p.getX()>this.getX() & p.getX()<this.destination.x & p.getY()>this.getY() & p.getY()<this.destination.y
+    //                 || p.getX()>this.getX() & p.getX()<this.destination.x & p.getY()<this.getY() & p.getY()>this.destination.y
+    //                 ) 
+    //                 {
+    //                     return true;                    
+    //                 }else
+    //                 {
+    //                     continue;
+    //                 }
+    //             }
+    //         }
+    //         return false;
+    //     }
 
     private void arrivedCheckPoint1()
     {
@@ -226,12 +292,14 @@ public class Passenger extends Actor
         int y= getY();
         if(x==checkPoint1.x & y==checkPoint1.y)
         {
-            targetPoint=checkPoint2;
+            targetPoint.x=checkPoint2.x;
+            targetPoint.y=checkPoint2.y;
             current_state=STATE_ArrivedPoint1;
         }
         else if(x==checkPoint2.x & y==checkPoint2.y)
         {
-            targetPoint=checkPoint3;
+            targetPoint.x=checkPoint3.x;
+            targetPoint.y=checkPoint3.y;
             current_state=STATE_ArrivedPoint2;
         }
         else if(x==checkPoint3.x & y==checkPoint3.y)
@@ -242,22 +310,15 @@ public class Passenger extends Actor
         }
         else if(x==targetPoint.x & y==targetPoint.y)
         {
-            if(targetPoint.x==destination.x & targetPoint.y==destination.y)
+            if(x==destination.x & y==destination.y)
             {
                 current_state=STATE_SEATED;
             }else
             {
-                current_state=STATE_WAITING;
+                current_state=STATE_WAITING2;
             }
 
         }
     }
 
-    public void moveOut(Passenger notifier)
-    {
-        this.notifier=notifier;
-        targetPoint.x=checkPoint3.x-1;
-        targetPoint.y=checkPoint3.y;        
-        this.current_state=STATE_MOVING;
-    }
 }
